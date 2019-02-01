3384ea1d4ddc7ee87ecb621e76714969f8607aab
Memo Akten <memo@memo.tv>
2019-02-01 04:52:02 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSAOpenCL_CV
+	ADDON_DESCRIPTION = Computer Vision with OpenCL
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/ofxMSAOpenCL_CV
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxMSAOpenCL
